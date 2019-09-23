987708a44550d2d9076b3d467d9f6fd07e83b6ce
Jonathan Ho <johnnyho82@gmail.com>
2019-09-23 17:52:07 -0400

    Commit before pulling from development branch

--- client/src/components/Child.js
+++ client/src/components/Child.js
@@ -1,5 +1,57 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import styled from 'styled-components';
+
+import { DataContext } from '../contexts/DataContext';
 
 export default function Child() {
-  return <div>Child</div>;
+  const { data, dispatchData } = useContext(DataContext);
+
+  return (
+    <ChildWrapper>
+      <h2>{data.child.name}</h2>
+      <div>
+        <p>Parent: {data.child.parentname}</p>
+        <p>Parent Contact: {data.child.parentcontact}</p>
+        <p>Date of Birth: {data.child.dob}</p>
+        <p>Gender: {data.child.gender}</p>
+      </div>
+      <table>
+        <caption>
+          <h3>Screenings:</h3>
+        </caption>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Weight</th>
+            <th>Height</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.child.screenings.map(el => (
+            <tr>
+              <th>{el.date}</th>
+              <th>{el.weight}</th>
+              <th>{el.height}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </ChildWrapper>
+  );
 }
+
+const ChildWrapper = styled.div`
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+
+  table {
+    font-family: inherit;
+    width: 90%;
+    max-width: 800px;
+    margin: 0 auto;
+    table-layout: fixed;
+    border-collapse: collapse;
+    border: 3px solid purple;
+  }
+`;
