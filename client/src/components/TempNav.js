2c1a51a42f4bbe411ccdd8ebe0117488d04b58ed
Jonathan Ho <johnnyho82@gmail.com>
2019-09-24 14:11:39 -0400

    Update NavBar styles

--- client/src/components/TempNav.js
+++ client/src/components/TempNav.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function TempNav() {
   return (
-    <div>
+    <NavBar>
       <NavLink to='/login'>Login</NavLink>
       <NavLink to='/register'>Register</NavLink>
       <NavLink to='/admin'>Admin</NavLink>
       {/* <NavLink to='/country'>Country</NavLink>
       <NavLink to='/community'>Community</NavLink>
       <NavLink to='/child'>Child</NavLink> */}
-    </div>
+    </NavBar>
   );
 }
+
+const NavBar = styled.div`
+  width: 280px;
+  margin: 0 auto 25px;
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    color: black;
+    text-decoration: none;
+    font-weight: 600;
+
+    :hover {
+      color: #0d71ba;
+    }
+  }
+`;
