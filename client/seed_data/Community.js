b4052c574d7513a12e887eb65dd336efb1b2e76a
zee <zzdd9251@gmail.com>
2019-09-25 12:21:14 -0700

    Backend data added..

--- client/seed_data/Community.js
+++ client/seed_data/Community.js
@@ -4,10 +4,16 @@ exports.seed = function(knex) {
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        { id: 1, name: 'Belem', country_id: 1 },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' },
+      return knex("table_name").insert([
+        { id: 1, name: "Belem", country_id: 1 },
+        { id: 2, name: "Fortaleza", country_id: 1 },
+        { id: 3, name: "Manaus", country_id: 1 },
+        { id: 4, name: "Sucre", country_id: 2 },
+        { id: 5, name: "Trinidad", country_id: 2 },
+        { id: 6, name: "El Torno", country_id: 2 },
+        { id: 7, name: "Krassang", country_id: 3 },
+        { id: 8, name: "Memot", country_id: 3 },
+        { id: 9, name: "Samraong", country_id: 3 }
       ]);
     });
 };
