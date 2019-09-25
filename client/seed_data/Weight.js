f29e1af481d8a534df085bb5ea0540b1cab27f83
Jonathan Ho <johnnyho82@gmail.com>
2019-09-25 16:11:16 -0400

    Set up Login form

--- client/seed_data/Weight.js
+++ client/seed_data/Weight.js
@@ -4,28 +4,26 @@ exports.seed = function(knex) {
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
-        { id: 1, child_id: 1, date: "04/12/2010", weight: 26.5 },
-        { id: 2, child_id: 1, date: "05/12/2010", weight: 27.2 },
-        { id: 3, child_id: 1, date: "10/05/2011", weight: 26.9 },
-        { id: 4, child_id: 2, date: "04/12/2010", weight: 35.4 },
-        { id: 5, child_id: 2, date: "05/12/2010", weight: 37.0 },
-        { id: 6, child_id: 2, date: "10/05/2011", weight: 38.1 },
-        { id: 7, child_id: 3, date: "04/12/2010", weight: 26.5 },
-        { id: 8, child_id: 3, date: "05/12/2010", weight: 27.2 },
-        { id: 9, child_id: 3, date: "10/05/2011", weight: 26.9 },
-        { id: 10, child_id: 4, date: "04/12/2010", weight: 35.4 },
-        { id: 11, child_id: 4, date: "05/12/2010", weight: 37.0},
-        { id: 12, child_id: 4, date: "10/05/2010", weight: 38.1 },
-        { id: 13, child_id: 5, date: "04/12/2010", weight: 26.5 },
-        { id: 14, child_id: 5, date: "05/12/2010", weight: 27.2 },
-        { id: 15, child_id: 5, date: "10/05/2011", weight: 26.9 },
-        { id: 16, child_id: 6, date: "04/12/2010", weight: 35.4 },
-        { id: 17, child_id: 6, date: "05/12/2010", weight: 37.0 },
-        { id: 18, child_id: 6, date: "10/05/2011", weight: 38.1},
-        { id: 19, child_id: 7, date: "04/12/2010", weight: 26.5 },
-
-     
+      return knex('table_name').insert([
+        { id: 1, child_id: 1, date: '04/12/2010', weight: 26.5 },
+        { id: 2, child_id: 1, date: '05/12/2010', weight: 27.2 },
+        { id: 3, child_id: 1, date: '10/05/2011', weight: 26.9 },
+        { id: 4, child_id: 2, date: '04/12/2010', weight: 35.4 },
+        { id: 5, child_id: 2, date: '05/12/2010', weight: 37.0 },
+        { id: 6, child_id: 2, date: '10/05/2011', weight: 38.1 },
+        { id: 7, child_id: 3, date: '04/12/2010', weight: 26.5 },
+        { id: 8, child_id: 3, date: '05/12/2010', weight: 27.2 },
+        { id: 9, child_id: 3, date: '10/05/2011', weight: 26.9 },
+        { id: 10, child_id: 4, date: '04/12/2010', weight: 35.4 },
+        { id: 11, child_id: 4, date: '05/12/2010', weight: 37.0 },
+        { id: 12, child_id: 4, date: '10/05/2010', weight: 38.1 },
+        { id: 13, child_id: 5, date: '04/12/2010', weight: 26.5 },
+        { id: 14, child_id: 5, date: '05/12/2010', weight: 27.2 },
+        { id: 15, child_id: 5, date: '10/05/2011', weight: 26.9 },
+        { id: 16, child_id: 6, date: '04/12/2010', weight: 35.4 },
+        { id: 17, child_id: 6, date: '05/12/2010', weight: 37.0 },
+        { id: 18, child_id: 6, date: '10/05/2011', weight: 38.1 },
+        { id: 19, child_id: 7, date: '04/12/2010', weight: 26.5 },
       ]);
     });
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { Redirect } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Form, Button, Input, theme } from '../styled-components/index';
 
+import { useUserContext } from '../contexts/UserContext';
+
 export default function Login() {
+  const { user, dispatch } = useUserContext();
+  const [credentials, setCredentials] = useState({
+    username: user.username,
+    password: user.password,
+    usertype: user.usertype,
+  });
+
+  const handleChange = e =>
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // axiosWithAuth()
+    //   .post('', credentials)
+    //   .then(res => {
+    //     console.log(res);
+    //   })
+    //   .catch(err => console.log(err));
+    console.log(credentials);
+  };
+
+  // Navigate to proper page given token and usertype
+  // if (localStorage.getItem('token')) return <Redirect to='/' />;
+
   return (
     <ThemeProvider theme={theme}>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <h3>Login:</h3>
         <span>
-          User
-          <Input type='radio' id='User' name='useType' value='user' />
-          Admin
-          <input type='radio' id='Admin' name='useType' value='admin' />
+          <label htmlFor='user'>User</label>
+          <input
+            type='radio'
+            id='user'
+            name='usertype'
+            value='user'
+            checked={credentials.usertype === 'user'}
+            onChange={handleChange}
+          />
+          <label htmlFor='admin'>Admin</label>
+          <input
+            type='radio'
+            id='admin'
+            name='usertype'
+            value='admin'
+            checked={credentials.usertype === 'admin'}
+            onChange={handleChange}
+          />
         </span>
 
         <label htmlFor='username'>Username</label>
@@ -20,6 +62,8 @@ export default function Login() {
           id='username'
           name='username'
           placeholder='Username'
+          value={credentials.username}
+          onChange={handleChange}
         />
 
         <label htmlFor='password'>Password</label>
@@ -28,6 +72,8 @@ export default function Login() {
           id='password'
           name='password'
           placeholder='Password'
+          value={credentials.password}
+          onChange={handleChange}
         />
 
         <Button type='submit'>Login</Button>
