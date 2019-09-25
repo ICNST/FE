35a83254d3133339530b562fd165f455902563c4
Jonathan Ho <johnnyho82@gmail.com>
2019-09-25 10:36:14 -0400

    Update DataContext.js

--- client/src/contexts/DataContext.js
+++ client/src/contexts/DataContext.js
@@ -1,4 +1,19 @@
-import { createContext } from 'react';
+import React, { createContext, useReducer, useContext } from 'react';
+import { initialState, dataReducer } from '../reducers/dataReducer';
 
 // This creates a context object to store and access child nutrition data records from the backend in state
 export const DataContext = createContext();
+
+export default function DataProvider({ children }) {
+  const [data, dispatchData] = useReducer(dataReducer, initialState);
+  return (
+    <DataContext.Provider value={{ data, dispatchData }}>
+      {children}
+    </DataContext.Provider>
+  );
+}
+
+export function useDataContext() {
+  const context = useContext(DataContext);
+  return context;
+}
