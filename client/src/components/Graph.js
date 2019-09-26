9602c5a6b5a928effdf1c0efcf13b857934862de
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-09-26 09:37:13 -0700

    Added responsiveness to graphs.js

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -7,7 +7,7 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  Legend,
+  ResponsiveContainer,
 } from 'recharts';
 
 export default function Graph(props) {
@@ -18,37 +18,34 @@ export default function Graph(props) {
       <h3>Graphs:</h3>
       <GraphWrapper>
         <h4>Weight (kg)</h4>
-        <LineChart
-          width={600}
-          height={300}
-          data={props.screenings.reverse()}
-          margin={{ top: 10, right: 40, left: 0, bottom: 10 }}>
-          <CartesianGrid stroke='#e6e6e6' strokeDasharray='5 5' />
-          <XAxis dataKey='date' />
-          <YAxis />
-          <Tooltip />
-          {/* <Legend /> */}
-          <Line
-            type='monotone'
-            dataKey='weight'
-            stroke='#0d71ba'
-            strokeWidth='2'
-            activeDot={{ r: 6 }}
-          />
-        </LineChart>
+        <ResponsiveContainer width='95%' height={300}>
+          <LineChart
+            data={props.screenings.reverse()}
+            margin={{ top: 10, right: 40, left: 0, bottom: 10 }}>
+            <CartesianGrid stroke='#e6e6e6' strokeDasharray='5 5' />
+            <XAxis dataKey='date' />
+            <YAxis />
+            <Tooltip />
+            <Line
+              type='monotone'
+              dataKey='weight'
+              stroke='#0d71ba'
+              strokeWidth='2'
+              activeDot={{ r: 6 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </GraphWrapper>
       <GraphWrapper>
         <h4>Height (cm)</h4>
+        <ResponsiveContainer width='95%' height={300}>
         <LineChart
-          width={600}
-          height={300}
           data={props.screenings}
           margin={{ top: 10, right: 40, left: 0, bottom: 10 }}>
           <CartesianGrid stroke='#e6e6e6' strokeDasharray='5 5' />
           <XAxis dataKey='date' />
           <YAxis />
           <Tooltip />
-          {/* <Legend /> */}
           <Line
             type='monotone'
             dataKey='height'
@@ -57,6 +54,7 @@ export default function Graph(props) {
             activeDot={{ r: 6 }}
           />
         </LineChart>
+        </ResponsiveContainer>
       </GraphWrapper>
     </ChartWrapper>
   );
@@ -66,6 +64,7 @@ const ChartWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
 `;
 
 const GraphWrapper = styled.div`
@@ -74,12 +73,18 @@ const GraphWrapper = styled.div`
   align-items: center;
   box-shadow: 1px 2px 3px #000;
   margin-bottom: 20px;
-
+  height: 300px;
+  width: 600px;
   h4 {
     background-color: #0d71ba;
     color: white;
     width: 100%;
     padding: 5px 0;
     margin-top: 0;
+  }
+
+  @media screen and (max-width: 620px) {
+    height: 45%;
+    width: 95%;
   }
 `;
